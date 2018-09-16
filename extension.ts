3d750b3eb8761aa0b03c0bafa1edc6473f412287
luqmaanb <lb@xinabox.cc>
2018-09-16 16:22:29 +0200

    .

---
+++ extension.ts
@@ -0,0 +1,10 @@
+namespace test {
+    /**
+     * Help goes here.
+     */
+    //% shim=test::extfun
+    export function extfun(x: number, y: number) {
+        // Dummy implementation for the simulator.
+        return x - y
+    }
+}
